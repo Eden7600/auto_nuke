0b7bbb395ee31de0a79ab0c8cca01e227f581e25
Adrian Irving-Beer <wisq@wisq.net>
2026-01-24 19:57:18 -0500

    `mix new auto_nuke`

---
+++ lib/auto_nuke.ex
@@ -0,0 +1,18 @@
+defmodule AutoNuke do
+  @moduledoc """
+  Documentation for `AutoNuke`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AutoNuke.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/auto_nuke_test.exs
@@ -0,0 +1,8 @@
+defmodule AutoNukeTest do
+  use ExUnit.Case
+  doctest AutoNuke
+
+  test "greets the world" do
+    assert AutoNuke.hello() == :world
+  end
+end
