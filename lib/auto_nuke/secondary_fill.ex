d69727c37d8af9253aab7909e92c2a3c5c4738c6
Adrian Irving-Beer <wisq@wisq.net>
2026-01-28 20:26:40 -0500

    Switch SecondaryFill to use ControlAxis.

--- lib/auto_nuke/secondary_fill.ex
+++ lib/auto_nuke/secondary_fill.ex
@@ -3,17 +3,16 @@ defmodule AutoNuke.SecondaryFill do
   require Logger
 
   defmodule State do
-    @enforce_keys [:loop, :pid, :speed, :fill_level]
+    @enforce_keys [:loop, :axis]
     defstruct(@enforce_keys)
   end
 
+  alias AutoNuke.ControlAxis
+
   @log_prefix "[#{inspect(__MODULE__)}] "
-  @loop_every 100
 
   @tank_size 60000.0
   @target_percent 0.5
-  @inlet_factor 2.0
-  @max_speed_deviation 20.0
 
   def start_link(opts) do
     {loop, opts} = Keyword.pop!(opts, :loop)
@@ -22,69 +21,45 @@ defmodule AutoNuke.SecondaryFill do
 
   @impl true
   def init(loop) when loop in 0..2 do
-    pid = PIDControl.new(kp: 1, kd: 0, ki: 0)
+    axis =
+      ControlAxis.new(
+        kp: 1,
+        ki: 0.1,
+        to_value_fn: &axis_to_speed/1,
+        offset: get_speed(loop) |> speed_to_axis()
+      )
 
     state = %State{
       loop: loop,
-      pid: pid,
-      speed: get_speed(loop),
-      fill_level: get_fill_percent(loop)
+      axis: axis
     }
 
-    Logger.info(@log_prefix <> "Started with fill level of #{state.fill_level * 100}%.")
-    {:ok, state, {:continue, :loop}}
+    PubSub.subscribe(self(), :ticker)
+
+    fill_level = get_fill_percent(loop)
+    Logger.info(@log_prefix <> "Started with fill level of #{fill_level * 100}%.")
+
+    {:ok, state}
   end
 
   @impl true
-  def handle_info(:loop, state) do
-    state =
-      get_fill_percent(state.loop)
-      |> update_fill_level(state)
+  def handle_info({:tick, _}, %State{loop: loop} = state) do
+    case ControlAxis.step(state.axis, @target_percent, get_fill_percent(loop)) do
+      {:changed, axis, new, old} ->
+        Logger.info(@log_prefix <> "Changing loop #{loop} speed from #{old} to #{new}.")
+        set_speed(loop, new)
+        axis
 
-    {:noreply, state, {:continue, :loop}}
-  end
-
-  @impl true
-  def handle_continue(:loop, state) do
-    Process.send_after(self(), :loop, @loop_every)
-    {:noreply, state}
-  end
-
-  defp update_fill_level(level, %State{fill_level: level} = state) do
-    # Nothing changed.
-    state
-  end
-
-  defp update_fill_level(new_level, %State{} = state) do
-    pid = state.pid |> PIDControl.step(@target_percent, new_level)
-    state = %State{state | pid: pid, fill_level: new_level}
-
-    new_speed =
-      (get_outlet(state.loop) / @inlet_factor)
-      |> adjust_pump_speed(pid.output)
-
-    if new_speed != state.speed do
-      Logger.info(@log_prefix <> "Setting loop #{state.loop} pump speed to #{new_speed}.")
-      set_speed(state.loop, new_speed)
-      %State{state | speed: new_speed}
-    else
-      state
+      {:unchanged, axis, _old_value} ->
+        axis
     end
-  end
-
-  defp adjust_pump_speed(speed, output) do
-    (speed + @max_speed_deviation * output)
-    |> round()
-    |> max(0)
-    |> min(100)
+    |> then(fn axis ->
+      {:noreply, %State{state | axis: axis}}
+    end)
   end
 
   defp get_fill_percent(loop) do
     AutoNuke.API.get_float("COOLANT_SEC_#{loop}_LIQUID_VOLUME") / @tank_size
-  end
-
-  defp get_outlet(loop) do
-    AutoNuke.API.get_float("STEAM_GEN_#{loop}_OUTLET")
   end
 
   defp get_speed(loop) do
@@ -94,4 +69,7 @@ defmodule AutoNuke.SecondaryFill do
   defp set_speed(loop, value) do
     AutoNuke.API.put("COOLANT_SEC_CIRCULATION_PUMP_#{loop}_ORDERED_SPEED", value)
   end
+
+  defp axis_to_speed(output), do: 50 + round(output * 50)
+  defp speed_to_axis(speed), do: (speed - 50) / 50
 end
