ee61176083aa79c3e47ddc9c7d161bf23d9a5d07
Adrian Irving-Beer <wisq@wisq.net>
2026-01-28 20:14:28 -0500

    Adjust Ticker to tick once per second, which seems to be the main update loop.

--- lib/auto_nuke/ticker.ex
+++ lib/auto_nuke/ticker.ex
@@ -3,8 +3,13 @@ defmodule AutoNuke.Ticker do
   require Logger
 
   @log_prefix "[#{inspect(__MODULE__)}] "
-  @tick_every 200
-  @pause_wait 100
+
+  # Track time in 200ms increments:
+  @loop_ms 200
+  # Emit ticks every 5 loops (1 second):
+  @tick_loops 5
+  # If paused, wait 50ms to check if unpaused:
+  @pause_wait 50
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, nil, opts)
@@ -14,20 +19,26 @@ defmodule AutoNuke.Ticker do
   def init(nil) do
     schedule_next()
     Logger.info(@log_prefix <> "Started ticking.")
-    {:ok, 0}
+    {:ok, {0, @tick_loops}}
   end
 
   @impl true
-  def handle_info(:tick, counter) do
+  def handle_info(:tick, {counter, 0}) do
     PubSub.publish(:ticker, {:tick, counter})
     schedule_next()
-    {:noreply, counter + 1}
+    {:noreply, {counter + 1, @tick_loops}}
   end
 
   @impl true
-  def handle_info(:paused, counter) do
+  def handle_info(:tick, {counter, loops}) when loops in 1..@tick_loops do
     schedule_next()
-    {:noreply, counter}
+    {:noreply, {counter, loops - 1}}
+  end
+
+  @impl true
+  def handle_info(:paused, state) do
+    schedule_next()
+    {:noreply, state}
   end
 
   defp get_sim_speed, do: AutoNuke.API.get_integer("GAME_SIM_SPEED")
@@ -39,7 +50,7 @@ defmodule AutoNuke.Ticker do
       Process.send_after(self(), :paused, @pause_wait)
     else
       interval =
-        @tick_every
+        @loop_ms
         |> div(speed)
         |> Kernel.-(lag)
         |> max(1)
