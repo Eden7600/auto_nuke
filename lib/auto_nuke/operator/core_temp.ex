f6abf46740b903b6cbc9a31472d0cd619894a550
Adrian Irving-Beer <wisq@wisq.net>
2026-01-30 01:03:43 -0500

    Fix core smoothing and rod inversion.

--- lib/auto_nuke/operator/core_temp.ex
+++ lib/auto_nuke/operator/core_temp.ex
@@ -50,7 +50,10 @@ defmodule AutoNuke.Operator.CoreTemp do
 
   @impl true
   def handle_cast({:target, t}, state) do
-    Logger.info("Target changed from #{state.target}°C to #{t}°C.")
+    Logger.info(
+      @log_prefix <> "Core #{state.core} target changed from #{state.target}°C to #{t}°C."
+    )
+
     {:noreply, %State{state | target: t}}
   end
 
@@ -76,9 +79,9 @@ defmodule AutoNuke.Operator.CoreTemp do
 
   defp get_smoothed_temperature(core, last_n) do
     temp = get_temperature(core)
-    last_n = [temp | Enum.take(last_n, @temp_count - 1)] |> Enum.sort()
+    last_n = [temp | Enum.take(last_n, @temp_count - 1)]
     middle_index = Enum.count(last_n) |> div(2)
-    median = last_n |> Enum.at(middle_index)
+    median = last_n |> Enum.sort() |> Enum.at(middle_index)
 
     {median, last_n}
   end
@@ -96,6 +99,6 @@ defmodule AutoNuke.Operator.CoreTemp do
     AutoNuke.API.put("ROD_BANK_POS_#{core - 1}_ORDERED", value)
   end
 
-  defp axis_to_rods(output), do: (50 + output * 50) |> Float.round(1)
-  defp rods_to_axis(rods), do: ((rods - 50) / 50) |> Float.round(5)
+  defp axis_to_rods(output), do: (50 - output * 50) |> Float.round(1)
+  defp rods_to_axis(rods), do: ((50 - rods) / 50) |> Float.round(5)
 end
