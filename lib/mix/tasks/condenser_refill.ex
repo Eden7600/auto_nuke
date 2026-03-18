feb3f47bd1774550471256ff0db0cb1ebb4984f7
Adrian Irving-Beer <wisq@wisq.net>
2026-03-18 18:20:46 -0400

    Rewrite condenser refill task.  Should probably be an operator.

--- lib/mix/tasks/condenser_refill.ex
+++ lib/mix/tasks/condenser_refill.ex
@@ -3,35 +3,55 @@ defmodule Mix.Tasks.AutoNuke.Condenser.Refill do
   @shortdoc "Refills condenser"
 
   use Mix.Task
+  alias AutoNuke.API
   alias AutoNuke.TaskUI, as: UI
 
   @tank_size 360_000
-  @default_target 0.5
+  @max div(@tank_size, 100)
 
-  def run([]), do: refill(@default_target)
+  def run([target]) do
+    target
+    |> String.to_integer()
+    |> refill()
+  end
 
-  def refill(target) when target >= 0.0 and target <= 1.0 do
+  def refill(target) when target >= 0 and target <= @max do
     {:ok, _} = Application.ensure_all_started([:req])
 
-    target_volume = @tank_size * target
-
-    if get_volume() <= target_volume do
-      set_switch(true)
+    try do
+      UI.set_wait_unless(
+        "Secondary Circuit Freight Pump",
+        "ON",
+        fn -> get_volume() >= target end,
+        fn -> get_active() end,
+        fn -> set_switch(true) end
+      )
 
       UI.progress_loop(
         label: "Condenser Level",
         fetch: &get_volume/0,
-        max: round(target_volume)
+        max: target
       )
+    after
+      UI.set_wait(
+        "Secondary Circuit Freight Pump",
+        "OFF",
+        fn -> !get_active() end,
+        fn -> set_switch(false) end
+      )
+
+      set_switch(false)
     end
 
-    set_switch(false)
     IO.puts("Done!")
   end
 
-  defp get_volume, do: AutoNuke.API.get_float("CONDENSER_VOLUME") |> round()
+  defp get_volume, do: API.get_float("CONDENSER_VOLUME") |> floor() |> div(100)
 
   @switch "FREIGHT_PUMP_CONDENSER_SWITCH"
-  defp set_switch(true), do: AutoNuke.API.put(@switch, "True")
-  defp set_switch(false), do: AutoNuke.API.put(@switch, "False")
+  defp set_switch(true), do: API.put(@switch, "True")
+  defp set_switch(false), do: API.put(@switch, "False")
+
+  @active "FREIGHT_PUMP_CONDENSER_ACTIVE"
+  defp get_active, do: API.get_boolean(@active)
 end
