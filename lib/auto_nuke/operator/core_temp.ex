cb0233d2f6e4ee3f8fac13a2693e18023b78d87d
Adrian Irving-Beer <wisq@wisq.net>
2026-03-30 14:50:35 -0400

    New CoreTemp implementation based on a fixed temperature range.
    All my previous attempts to manage heat flow assumed that taking more or
    less heat from the reactor was a sustainable affair, which in turn
    relied on my old control rod automation that would do whatever it took
    to keep core temperature stable.
    
    Since we've abandoned that method, it's time to abandon micro-managing
    core heat flow, too.  Keeping core factor stable means we're always
    generating roughly the same amount of heat, and any attempts to move it
    faster or slower will be temporary — moving hot water faster (to get
    more energy) will soon turn into moving cold water faster (less energy
    again), and vice versa, and on a timescale that is too short to be
    worthwhile for managing demand.
    
    So now, we just scale the pumps based on core temperature.  Higher
    temperature, more pumps.  But we don't need a PID controller — instead,
    it's just a fixed value based on temperature (low at low temps, high at
    high temps) with a wide temperature range (320°C to 400°C).
    
    This has the effect of simply dampening the core factor's effect on
    temperature.  More heat produced = more heat sent to steam generators =
    core temperature stays more steady.  Essentially it turns higher
    reactivity into higher heat flow, while reducing core temperature
    variability.

--- lib/auto_nuke/operator/core_temp.ex
+++ lib/auto_nuke/operator/core_temp.ex
@@ -3,104 +3,80 @@ defmodule AutoNuke.Operator.CoreTemp do
   require Logger
 
   defmodule State do
-    @enforce_keys [:target, :axis]
+    @enforce_keys [:pump_speed, :smoothed_temp]
     defstruct(@enforce_keys)
   end
 
-  alias AutoNuke.ControlAxis
   alias AutoNuke.API
+  alias AutoNuke.Smoother
 
   @log_prefix "[#{inspect(__MODULE__)}] "
 
-  # We'll use the raw indices here for convenience.
-  @loops 0..2
+  # Allowed pump speeds.
   # I'm told that <10% is dangerous and >50% is useless.
-  # Also, there's the "keep pumps below 50%" objective.
-  @speeds 10..49
-  @speed_span (@speeds.last - @speeds.first) / 2
+  # Also, there's the "keep pumps below 50%" objective to think about.
+  @pump_min 10
+  @pump_max 49
+  # Scale pumps from @pump_min speed at @temp_min, to @pump_max speed at @temp_max.
+  @temp_min 320
+  @temp_max 400
+  # Use the average temperature from the last five in-game minutes.
+  @temp_smoothing AutoNuke.Ticker.ticks_per_minute() * 5
+
+  # We'll use the raw indices here for convenience, since they aren't shown to the user.
+  @loops 0..2
 
   def start_link(opts \\ []) do
-    {target, opts} = Keyword.pop(opts, :target)
-    opts = Keyword.put_new(opts, :name, __MODULE__)
-    GenServer.start_link(__MODULE__, target, opts)
-  end
-
-  def set_target(target, pid \\ __MODULE__) do
-    GenServer.cast(pid, {:target, target})
+    GenServer.start_link(__MODULE__, nil, opts)
   end
 
   @impl true
-  def init(target) when is_number(target) or is_nil(target) do
-    temp = get_temperature()
+  def init(_) do
     speed = get_average_pump_speed()
-    target = target || get_initial_target(speed, temp)
-
-    axis =
-      ControlAxis.new(
-        kp: -0.01,
-        ki: -0.001,
-        deadzone: 0.5,
-        to_value_fn: &axis_to_speed/1,
-        offset: speed |> speed_to_axis(),
-        initial_value: speed
-      )
+    temp = get_temperature()
+    smoother = Smoother.new(@temp_smoothing) |> Smoother.add(temp)
 
     state =
       %State{
-        target: target,
-        axis: axis
+        pump_speed: speed,
+        smoothed_temp: smoother
       }
 
     PubSub.subscribe(self(), :ticker)
-
-    Logger.info(
-      @log_prefix <> "Started with temperature #{temp}°C, target #{target}°C, pumps at #{speed}."
-    )
-
+    Logger.info(@log_prefix <> "Started with temperature #{temp}°C, pumps at #{speed}.")
     {:ok, state}
   end
 
   @impl true
-  def handle_cast({:target, t}, %State{} = state) do
-    Logger.info(
-      @log_prefix <> "Core temperature target changed from #{state.target}°C to #{t}°C."
-    )
-
-    {:noreply, %State{state | target: t}}
-  end
-
-  @impl true
   def handle_info({:tick, _}, %State{} = state) do
-    temp = get_temperature()
+    smoother = state.smoothed_temp |> Smoother.add(get_temperature())
+    temp = Smoother.average(smoother)
 
-    case ControlAxis.step(state.axis, state.target, temp) do
-      {:changed, axis, new, old} ->
-        Logger.info(@log_prefix <> "Changing pump speeds from #{old} to #{new}.")
-        set_pump_speeds(new)
-        axis
+    old = state.pump_speed
+    new = temp_to_speed(temp)
 
-      {:unchanged, axis, _old_value} ->
-        axis
+    if new != old do
+      Logger.info(@log_prefix <> "Changing pump speeds from #{old} to #{new}.")
+      set_pump_speeds(new)
     end
-    |> then(fn axis ->
-      {:noreply, %State{state | axis: axis}}
-    end)
+
+    {:noreply, %State{state | pump_speed: new, smoothed_temp: smoother}}
   end
 
   defp get_average_pump_speed do
     @loops
     |> Enum.map(&get_pump_speed/1)
-    |> Enum.reject(&(&1 < @speeds.first))
+    |> Enum.reject(&(&1 < @pump_min))
     |> then(fn
-      # Will average out to mid-speed.
-      # Seems like a reasonable place to start if we can't find any usable speeds.
-      [] -> [@speeds.first, @speeds.last]
+      # If all pumps are below minimum, then just use that.
+      # (I don't forsee ever having pumps above maximum.)
+      [] -> [@pump_min]
       speeds when is_list(speeds) -> speeds
     end)
     |> Statistex.average()
   end
 
-  defp set_pump_speeds(speed) when speed in @speeds do
+  defp set_pump_speeds(speed) when speed >= @pump_min and speed <= @pump_max do
     @loops |> Enum.each(&set_pump_speed(&1, speed))
   end
 
@@ -109,12 +85,15 @@ defmodule AutoNuke.Operator.CoreTemp do
   defp get_pump_speed(n), do: API.get_float("COOLANT_CORE_CIRCULATION_PUMP_#{n}_SPEED")
   defp set_pump_speed(n, v), do: API.put("COOLANT_CORE_CIRCULATION_PUMP_#{n}_ORDERED_SPEED", v)
 
-  defp axis_to_speed(output), do: round((output + 1.0) * @speed_span + @speeds.first)
-  defp speed_to_axis(speed), do: (speed - @speeds.first) / @speed_span - 1.0
+  @temp_span @temp_max - @temp_min
+  @pump_span @pump_max - @pump_min
 
-  # If speed is low, assume it's because our old target was too high.
-  defp get_initial_target(speed, temp) when speed == @speeds.first, do: temp + 10
-  # If speed is high, assume it's because our old target was too low.
-  defp get_initial_target(speed, temp) when speed == @speeds.last, do: temp - 10
-  defp get_initial_target(_, temp), do: temp
+  defp temp_to_speed(temp) do
+    percent_in_range = (temp - @temp_min) / @temp_span
+
+    (@pump_min + @pump_span * percent_in_range)
+    |> round()
+    |> max(@pump_min)
+    |> min(@pump_max)
+  end
 end
