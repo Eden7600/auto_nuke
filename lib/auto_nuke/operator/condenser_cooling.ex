fd66b8d8291ad1bca9d15c315b1673625256f823
Adrian Irving-Beer <wisq@wisq.net>
2026-03-31 12:49:00 -0400

    New condenser cooling handler.  Now uses probe-backoff strategy.
    The threshold is still 5°C above ambient, but instead of trying to
    PI-control that, we just probe downwards very slowly over time, then
    back off hard and wait a while when temperature exceeds the threshold.
    
    Due to the backoff periods — we avoid probing for 15 minutes after a
    violation, we typically typically bump speed by +3% on violation, and we
    drop speed by -1% every 5 minutes while probing — violations will
    typically occur every half hour or so.  That's slow enough that I don't
    consider it needlessly aggressive or particularly spammy.

--- lib/auto_nuke/operator/condenser_cooling.ex
+++ lib/auto_nuke/operator/condenser_cooling.ex
@@ -3,33 +3,22 @@ defmodule AutoNuke.Operator.CondenserCooling do
   require Logger
 
   defmodule State do
-    @enforce_keys [:axis, :last_temp]
+    @enforce_keys [:speed, :last_temp, :last_direction, :probe_timer]
     defstruct(@enforce_keys)
   end
 
   alias AutoNuke.API
-  alias AutoNuke.ControlAxis
 
   @log_prefix "[#{inspect(__MODULE__)}] "
 
   # Range of allowed speeds.
   @speeds 10..100
-  @speed_span (@speeds.last - @speeds.first) / 2
-  # Target 5°C above ambient.
+  # Violation threshold is 5°C above ambient.
   @above_ambient 5.0
-
-  # Low-pass filter quotient.  Must be between 0.0 and 1.0.
-  # Filters out high-frequency noise in the temperature,
-  # e.g. the erroneous readings we sometimes get for a single reading.
-  # Set higher for more smoothing, lower for less.
-  @lpf_factor 0.75
-  # The pump can only scale up so fast.
-  # If we start asking for pump speeds that are 2+ above our current speed,
-  # clamp down to prevent integral windup.
-  @clamping 2
-
-  defp axis_to_speed(output), do: round((output + 1.0) * @speed_span + @speeds.first)
-  defp speed_to_axis(speed), do: (speed - @speeds.first) / @speed_span - 1.0
+  # Wait 15 in-game minutes after a violation to begin probing lower speeds:
+  @wait_after_violation 15 * AutoNuke.Ticker.ticks_per_minute()
+  # While probing, wait five in-game minutes per 1% speed drop:
+  @wait_while_probing 5 * AutoNuke.Ticker.ticks_per_minute()
 
   def start_link(opts \\ []) do
     opts = Keyword.put_new(opts, :name, __MODULE__)
@@ -41,17 +30,12 @@ defmodule AutoNuke.Operator.CondenserCooling do
     temp = get_temperature()
     speed = get_pump_speed()
 
-    axis =
-      ControlAxis.new(
-        kp: -0.01,
-        ki: -0.005,
-        deadzone: 0.5,
-        to_value_fn: &axis_to_speed/1,
-        offset: speed |> speed_to_axis(),
-        initial_value: speed
-      )
-
-    state = %State{axis: axis, last_temp: temp}
+    state = %State{
+      speed: speed,
+      last_temp: temp,
+      last_direction: :stable,
+      probe_timer: @wait_while_probing
+    }
 
     PubSub.subscribe(self(), :ticker)
     Logger.info(@log_prefix <> "Started with temperature #{temp}°C, pump at #{speed}.")
@@ -60,41 +44,84 @@ defmodule AutoNuke.Operator.CondenserCooling do
 
   @impl true
   def handle_info({:tick, _}, %State{} = state) do
-    temp = get_temperature() |> low_pass_filter(state.last_temp)
-    target = get_ambient() + @above_ambient
+    new_temp = get_temperature()
+    old_temp = state.last_temp
+    threshold = get_ambient() + @above_ambient
 
-    case ControlAxis.step(state.axis, target, temp) do
-      {:changed, new_axis, new, old} ->
-        Logger.info(@log_prefix <> "Changing pump speed from #{old} to #{new}.")
-        set_pump_speed(new)
-        maybe_clamp(new_axis, new)
+    status =
+      cond do
+        new_temp >= threshold -> :above
+        new_temp < threshold -> :below
+      end
 
-      {:unchanged, new_axis, _old_value} ->
-        new_axis
+    direction =
+      cond do
+        abs(new_temp - old_temp) < 0.01 -> :stable
+        new_temp > old_temp -> :increasing
+        new_temp < old_temp -> :decreasing
+      end
+
+    # We track both the current direction and the previous direction,
+    # to avoid momentary reading glitches from throwing us off.
+
+    case {status, direction, state.last_direction} do
+      # === Above threshold: ===
+      # - Steadily increasing: Backoff by 3%.
+      {:above, :increasing, :increasing} -> backoff(state, 3, new_temp)
+      # - Decreasing: Good, but hold off on probing until we're safe.
+      {:above, :decreasing, _} -> probe_wait(state, @wait_after_violation)
+      {:above, _, :decreasing} -> probe_wait(state, @wait_after_violation)
+      # - Any other situation: Let's back off to get it below the threshold.
+      {:above, _, _} -> backoff(state, 1, new_temp)
+      # 
+      # === Below threshold: ===
+      # - Steadily increasing: Hold off on probing.
+      {:below, :increasing, :increasing} -> probe_wait(state, @wait_while_probing)
+      # - Recent increase: Skip the timer for this tick.
+      {:below, :increasing, _} -> state
+      {:below, _, :increasing} -> state
+      # - No recent increases: Begin probing downwards.
+      {:below, _, _} -> maybe_probe(state, new_temp)
     end
-    |> then(fn %ControlAxis{} = new_axis ->
-      {:noreply, %State{state | axis: new_axis, last_temp: temp}}
+    |> then(fn %State{} = new_state ->
+      {:noreply, %State{new_state | last_temp: new_temp, last_direction: direction}}
     end)
   end
 
-  defp low_pass_filter(new, old), do: (1 - @lpf_factor) * new + @lpf_factor * old
+  defp backoff(%State{} = state, amount, temp) do
+    old = state.speed
+    new = (get_pump_speed() + amount) |> min(@speeds.last)
 
-  defp maybe_clamp(axis, ordered) do
-    actual = get_pump_speed()
+    Logger.info(
+      @log_prefix <> "Temperature violation at #{temp}°C, backing off from #{old} to #{new}."
+    )
 
-    if ordered - actual > @clamping do
-      # We're asking for too much pump speed at once, clamp down.
-      Logger.debug(@log_prefix <> "Clamping down to #{actual + @clamping}")
-      clamp_to = (actual + @clamping) |> speed_to_axis()
-      axis |> ControlAxis.clamp_min(clamp_to)
-    else
-      axis
-    end
+    set_pump_speed(new)
+    %State{state | speed: new, probe_timer: @wait_after_violation}
+  end
+
+  defp probe_wait(%State{} = state, wait) do
+    %State{state | probe_timer: max(state.probe_timer, wait)}
+  end
+
+  defp maybe_probe(%State{probe_timer: timer} = state, _) when timer > 0 do
+    %State{state | probe_timer: timer - 1}
+  end
+
+  defp maybe_probe(%State{speed: speed} = state, _) when speed <= @speeds.first do
+    state
+  end
+
+  defp maybe_probe(%State{} = state, temp) do
+    new = state.speed - 1
+    Logger.info(@log_prefix <> "Temperature steady at #{temp}°C, probing down to #{new}.")
+    set_pump_speed(new)
+    %State{state | speed: new, probe_timer: @wait_while_probing}
   end
 
   defp get_temperature, do: API.get_float("CONDENSER_TEMPERATURE")
   defp get_ambient, do: API.get_float("AMBIENT_TEMPERATURE")
 
-  defp get_pump_speed, do: API.get_float("CONDENSER_CIRCULATION_PUMP_SPEED")
+  defp get_pump_speed, do: API.get_float("CONDENSER_CIRCULATION_PUMP_SPEED") |> round()
   defp set_pump_speed(v), do: API.put("CONDENSER_CIRCULATION_PUMP_ORDERED_SPEED", v)
 end
