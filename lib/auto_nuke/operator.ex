ddf24813b71428384ad732c369f5ec1e752d300b
Adrian Irving-Beer <wisq@wisq.net>
2026-06-13 08:47:40 -0400

    Reorder operators and add new definitions.
    I'm revisiting this project after stepping away for a while, and I'm
    finding that my working tree has a lot of changes, including several new
    operators.  I'm splitting each change and new operator into logical
    commits, but many of they depend on adding new definitions in Operator,
    and I don't feel like making temporary changes to that file to
    accomodate that.
    
    Since adding unused definitions doesn't break anything, here's the
    complete current version of Operator, with the actual new operators to
    follow in separate commits.

--- lib/auto_nuke/operator.ex
+++ lib/auto_nuke/operator.ex
@@ -1,16 +1,26 @@
 defmodule AutoNuke.Operator do
   alias __MODULE__, as: Op
 
-  def assigned_tick(Op.CoreFactor), do: 0
+  # Core concerns:
   def assigned_tick(Op.SteamFlow), do: 0
+  # Depends on SteamFlow data:
   def assigned_tick(Op.CorePower), do: 1
+  # Is assigned targets by CorePower:
   def assigned_tick(Op.CoreTemp), do: 2
-  def assigned_tick(Op.SecondaryFill), do: 3
-  def assigned_tick(Op.VacuumTank), do: 3
-  def assigned_tick(Op.CondenserFill), do: 4
+  # Depends on CoreTemp data:
+  def assigned_tick(Op.AutoDrift), do: 3
+  # Is assigned drifts by AutoDrift:
+  def assigned_tick(Op.CoreFactor), do: 4
+
+  # Auxiliary concerns:
+  def assigned_tick(Op.CoreFill), do: 0
+  def assigned_tick(Op.SecondaryFill), do: 1
+  def assigned_tick(Op.VacuumTank), do: 2
+  def assigned_tick(Op.CondenserFill), do: 3
   def assigned_tick(Op.CondenserCooling), do: 4
 
   def assigned_tick_modulo(Op.CorePower), do: 40
+  def assigned_tick_modulo(Op.AutoDrift), do: 40
   def assigned_tick_modulo(_), do: 5
 
   defmacro __using__(_) do
